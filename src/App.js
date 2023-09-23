790461340357daf88c541bffd98d966509d84087
Piyush Sachdeva <piyushsachdeva@Piyushs-MacBook-Air.local>
2023-09-23 12:06:44 +0530

    changesss

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       Deployment successful beta version!
+       Deployment successful beta versionsssss!
       </header>
     </div>
   );
